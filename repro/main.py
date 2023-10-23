51392dc12976c48e9b61419dcbedfea9f26d5ee2
Philip Linden <45497023+philiplinden@users.noreply.github.com>
2023-10-23 00:00:13 -0400

    Replicate Matlab functions (#1)
    * CHECKPOINT: placeholder classes
    
    * SSA
    
    * link to matlab functions
    
    * only support python 3.11

--- repro/main.py
+++ repro/main.py
@@ -1,9 +1,17 @@
-import .hapke
+from matplotlib import pyplot as plt
+import numpy as np
+from pprint import pprint as print
 
-
-class Hydrated_Regolith_Spectra_Generator_and_Retrieval:
-    pass
+from simulator import Hapke, get_sample_grid
 
 
 if __name__ == '__main__':
-    pass
+    WLS = get_sample_grid()
+    Refl = np.array([np.random.randn()*x for x in WLS])
+    R = Hapke(Refl, WLS)
+    print(WLS)
+    print(Refl)
+    print(R)
+    plt.figure()
+    plt.plot(WLS,Refl,WLS,R)
+    plt.show()
---
+++ repro/spectra.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+
+@dataclass
+class Endmember:
+    name: str
+    abundance_range: Tuple[float, float]
+    max_abundance: float
+    density: float
+    grain_size_range: Tuple[float, float]
+
+
+class HydratedMorbGlass(Endmember):
+    ''' Hydrated mid-ocean-ridge basalt (MORB) glass '''
+    sample_label = ''
+    spectrum_label = 'MORB D38A'
+    density = 2.8
+    grain_size = 69e-6
+
+
+class Regolith(Endmember):
+    density = 1.8
+    grain_size = 32e-6
