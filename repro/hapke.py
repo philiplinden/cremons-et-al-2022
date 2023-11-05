96b18864f0c000704854d4fe11dfc96944112318
Philip Linden <lindenphilipj@gmail.com>
2023-11-05 13:46:28 -0500

    beginnings of figure 2

--- repro/hapke.py
+++ repro/hapke.py
@@ -7,14 +7,11 @@ This module emulates the behavior of the following MATLAB functions:
     src/Hapke_Lidar_SSA_Function.m
     src/Hydrated_Regolith_Spectra_Generator_and_Retrieval.m
 """
-from collections import namedtuple
 from functools import partial
-from typing import Callable
-import numpy as np
-from pandas import Series
-from scipy.optimize import fmin
+from typing import Callable, Tuple
 
-HFunction = namedtuple('HFunction', 'H H0')
+import numpy as np
+from scipy.optimize import fmin
 
 
 def ordinary_least_squares(x: float, y: Callable, yx: float) -> float:
@@ -27,7 +24,7 @@ def ordinary_least_squares(x: float, y: Callable, yx: float) -> float:
     return sum((y(x) - yx) ** 2)
 
 
-def angular_width(filling_factor):
+def angular_width(filling_factor: float) -> float:
     """Angular width parameter, see Equation 3.
 
     Args:
@@ -48,14 +45,21 @@ def backscattering(h: float, g: float) -> float:
     return 1 / (1 + (1 / h) * np.tan(g / 2))
 
 
-def bidirectional_reflectance(
+def reflectance_from_ssa(
     SSA: float, P: float, mu: float, mu0: float, B: float
 ) -> float:
     """Estimate bidirectional reflectance from single-scattering albedo.
-    
+
+    This function applies the Hapke model to estimate reflectance from
+    single-scattering albedo and other characteristics of a mixture and
+    spectral observation parameters.
+
     see Equation 1 in the manuscript:
 
         R = (ω/4) (μ₀ / (μ + μ₀)) {(1 + B)P + H(ω)H₀(ω) - 1}
+
+    Hapke, B. (2012). Theory of reflectance and emittance spectroscopy
+        (2nd ed.). Cambridge University Press.
 
     I broke this equation down into smaller terms for code readability.
         let:
@@ -80,7 +84,7 @@ def bidirectional_reflectance(
         float: bidrectional reflectance
     """
 
-    def h_func(SSA: float, mu: float, mu0: float) -> HFunction:
+    def h_func(SSA: float, mu: float, mu0: float) -> Tuple[float, float]:
         """Ambartsumian-Chandrasekhar H functions.
 
         Computed using the approximation from equation 8.57 from Hapke (2012).
@@ -89,8 +93,11 @@ def bidirectional_reflectance(
             SSA (float): single-scattering albedo, aka ω
             mu (float): cosine of emission angle
             mu0 (float): coside of incident angle
+
+        Returns:
+            H, H0 (Tuple[float, float]): Ambartsumian-Chandrasekhar H-functions
         """
-        gamma = np.sqrt(1 - SSA)
+        gamma = np.sqrt(max(1 - SSA, 0))
         r0 = (1 - gamma) / (1 + gamma)
 
         h1 = np.log((1 + mu0) / mu0)
@@ -103,7 +110,7 @@ def bidirectional_reflectance(
         h5 = r0 + h3 * (h4 / 2)
 
         H0 = (1 - SSA * mu * h5) ** -1
-        return HFunction(H, H0)
+        return H, H0
 
     H, H0 = h_func(SSA, mu, mu0)
 
@@ -114,19 +121,20 @@ def bidirectional_reflectance(
     return r1 * r2 * (r3 + r4 - 1)
 
 
-def reflectance_to_ssa(
-    reflectance: Series,
+def ssa_from_reflectance(
+    reflectance: float,
     asymmetry_factor: float = 0.81,
     emission_angle: float = 0,
     incident_angle: float = 30,
     phase_angle: float = 30,
     filling_factor: float = 0.41,
-):
+    initial_guess: float = 0.5,
+) -> float:
     """Estimate single-scattering albedo (SSA) from reflectance
 
     TLDR; uses ordinary least squares to fit an SSA that produces the observed
     reflectance when the SSA is fed back through the Hapke model.
-    
+
     Equivalent to src/Hapke_Lidar_SSA_function.m
 
     Uses scipy.optimize.fmin (equivalent to MATLAB fminsearch) to minimize
@@ -134,52 +142,43 @@ def reflectance_to_ssa(
     reflectance, R, and the SSA from the estimated reflectance at each
     sample point in WLS.
 
-    Hapke, B. (2012). Theory of reflectance and emittance spectroscopy
-        (2nd ed.). Cambridge University Press.
-
     Default parameter values replicate src/Hapke_Inverse_Function_Passive.m
 
     Args:
-        reflectance (Series): Bidirectional reflectance, R. see Equation 1.
+        reflectance (float): Bidirectional reflectance, R. see Equation 1.
+        wavelength (float): Wavelength in microns.
         asymmetry_factor (float, optional): Scattering asymmetry factor, p.
         emission_angle (float, optional): Emission angle in degrees.
             Defaults to 0.
         incident_angle (float, optional): Incident angle in degrees.
             Defaults to 30.
         phase_angle (float, optional): Phase angle in degrees. Defaults to 30.
-        filling_factor (float, optional: Particle filling factor.
+        filling_factor (float, optional): Particle filling factor.
             Defaults to 0.41.
+        initial_guess (float, optional): initial guess of SSA, ω₀
     """
     mu = np.cos(np.deg2rad(emission_angle))
     mu0 = np.cos(np.deg2rad(incident_angle))
     g = np.deg2rad(phase_angle)
     h = angular_width(filling_factor=filling_factor)
     B = backscattering(h, g)
+    w0 = initial_guess
 
-    w = []
-    for index, value in reflectance.items():
-        w0 = 0.5  # initial guess of SSA, ω₀
+    # turn reflectance_from_ssa() into the form y=f(x)
+    y = partial(reflectance_from_ssa, P=asymmetry_factor, mu=mu, mu0=mu0, B=B)
 
-        # turn bidrectional_reflectance() into the form y=f(x)
-        y = partial(
-            bidirectional_reflectance, P=asymmetry_factor, mu=mu, mu0=mu0, B=B
-        )
+    # formulate ordinary least squares between estimated reflectance, y
+    # and observed reflectance, yx, and arrange into the form y=f(x)
+    OLS = partial(ordinary_least_squares, y=y, yx=reflectance)
 
-        # formulate ordinary least squares between estimated reflectance, y
-        # and observed reflectance, yx, and arrange into the form y=f(x)
-        OLS = partial(
-            ordinary_least_squares, y=y, yx=value
-        )
-        w.append(
-            fmin(
-                OLS,
-                w0,
-                args=index,
-                disp=False,
-                maxiter=10_000,
-                maxfun=10_000,
-                ftol=1e-7,
-                xtol=1e-7,
-            )
-        )
-    return w
+    # find SSA that minimizes least squares difference between observed
+    # reflectance and estimated (Hapke) reflectance
+    opts = dict(
+        disp=False,
+        maxiter=10_000,
+        maxfun=10_000,
+        ftol=1e-7,
+        xtol=1e-7,
+    )
+    result = fmin(OLS, w0, **opts)
+    return result[0]
